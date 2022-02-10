d5b984cf674a398694921977831affbc265b1862
Rahul Singh <rstraining4@gmail.com>
2022-02-10 20:24:22 +0000

    tf

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('<h1 style="color:green;">Sample Node App - version-1!!</h1> \n');
+res.send('<h1 style="color:green;">This is first ECS Service deployment by Rahul Singh</h1> \n');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
